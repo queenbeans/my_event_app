cb1f4566b71807cf7adb0f37c9611ea0cb34ccbf
Davian Flores <davian@rentaba.co>
2026-02-11 17:39:06 -0500

    Add validation and scopes to my Active Record model to handle requirements

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+    validates :title, :start_time, :end_time, presence: true
+    validates :end_time, comparison: { greater_than: :start_time}
+    
+    scope :published_events, -> { where(published: true)}
+    scope :upcoming, -> { where("end_time >= ?", Time.current)}
+    scope :past, -> { where("end_time < ?", Time.current)}
+    scope :by_start_time, -> {order(start_time: :asc)}
 end
