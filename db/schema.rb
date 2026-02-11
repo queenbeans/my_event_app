2d747ad041dfb7d936433de0b50533b839a8eca0
Davian Flores <davian@rentaba.co>
2026-02-11 17:10:03 -0500

    undo migration. Will run scaffold instead.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_11_215322) do
-  create_table "events", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.text "description"
-    t.datetime "end_time", null: false
-    t.string "location"
-    t.boolean "published", default: false
-    t.datetime "start_time", null: false
-    t.string "title", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[8.1].define(version: 0) do
 end
