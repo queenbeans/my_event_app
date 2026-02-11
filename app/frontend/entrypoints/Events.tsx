fc172351fc7d73a26d4b2e692a914535dfa9e826
Davian Flores <davian@rentaba.co>
2026-02-11 17:56:12 -0500

    Add apollo to FE for querying purposes

--- app/frontend/entrypoints/Events.tsx
+++ app/frontend/entrypoints/Events.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Events } from '../components/Events/Events';
 import ErrorBoundary from '../components/ErrorBoundary';
+import { ApolloProvider } from '@apollo/client/react';
+import client from '../utils/apolloClient';
 
 const App = () => (
   <ErrorBoundary>
-    <Events />
+    <ApolloProvider client={client}>
+      <Events />
+    </ApolloProvider>
   </ErrorBoundary>
 );
 
